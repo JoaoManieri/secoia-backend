cfd8833286e6a3c55df24f6b6efa1fcd7be5708a
Davi Ponce Manieri <dvponce3@gmail.com>
2023-06-15 22:06:04 -0300

    alteraçao tela e teste

--- src/main/java/com/manieri/projetosequoia/ui/costumers/EditCostumerControler.java
+++ src/main/java/com/manieri/projetosequoia/ui/costumers/EditCostumerControler.java
@@ -1,4 +1,30 @@
 package com.manieri.projetosequoia.ui.costumers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class EditCostumerControler {
+
+    @FXML
+    private Label Jlabel;
+    @FXML
+    private TextField JText;
+
+    @FXML
+    protected void escreva() {
+        System.out.println("Davi");
+        Jlabel.setText(JText.getText());
+
+
+
+    }
+
+
+
+
+
 }
+
+
